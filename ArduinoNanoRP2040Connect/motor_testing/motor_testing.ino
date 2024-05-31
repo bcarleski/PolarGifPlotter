dab6e591fcf69d1c473a222968b6d452c4219689
Benjamin Carleski <carleski@amazon.com>
2024-05-31 10:05:07 -0700

    Update motor_testing.ino

--- ArduinoNanoRP2040Connect/motor_testing/motor_testing.ino
+++ ArduinoNanoRP2040Connect/motor_testing/motor_testing.ino
@@ -21,13 +21,17 @@
 // #define RADIUS_STEPPER_DIR_PIN 17
 // #define AZIMUTH_STEPPER_STEP_PIN 18
 // #define AZIMUTH_STEPPER_DIR_PIN 19
-#define MS1_PIN 14
-#define MS2_PIN 15
+// #define MS1_PIN 14
+// #define MS2_PIN 15
 #define STEP_PIN 16
 #define DIR_PIN 17
 #define FAST_OFFSET 110  // LOW, LOW, 110
 #define SLOW_OFFSET 550  // LOW, HIGH, 550
+#define SERIAL_PORT Serial1 // HardwareSerial port pins 0 & 1
+#define DRIVER_ADDRESS 0b00
+#define R_SENSE 0.11f
 
+#include <SpeedyStepper.h>
 #include <TMCStepper.h>
 
 const uint8_t REPLY_DELAY = 4;
@@ -38,23 +42,27 @@ unsigned long nextStepUpdateTime = 0;
 unsigned long nextPositionUpdateTime = 0;
 unsigned long nextSerialPollTime = 0;
 unsigned long nextStepTime = 0;
-unsigned long nextStepTimeOffset = SLOW_OFFSET;
+unsigned long nextStepTimeOffset = 4000;
 unsigned long currentStep = 0;
-long stepLimit = 20000;
+long stepLimit = 2000;
 long stepNumber = 0;
+long gapWaitTime = 1000;
 bool adjustingAzimuth = false;
 bool running = false;
-bool reverse = false;
+bool shaft = false;
+bool direction = false;
 
-TMC2209Stepper driver(&Serial1, 0.11f, 0b00);
+TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
+
+//SpeedyStepper stepper;
 
 void setup() {
-  pinMode(MS1_PIN, OUTPUT);
-  pinMode(MS2_PIN, OUTPUT);
+  // pinMode(MS1_PIN, OUTPUT);
+  // pinMode(MS2_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
 
-  Serial1.begin(115200);
+  SERIAL_PORT.begin(115200);
   Serial.begin(115200);
   unsigned long start = millis();
 
@@ -66,24 +74,74 @@ void setup() {
 
   delay(2000);
 
-  digitalWrite(MS1_PIN, LOW);
-  digitalWrite(MS2_PIN, LOW);
+  // digitalWrite(MS1_PIN, LOW);
+  // digitalWrite(MS2_PIN, LOW);
   digitalWrite(STEP_PIN, LOW);
   digitalWrite(DIR_PIN, LOW);
   driver.begin();
+  driver.reset();
   driver.toff(5);
-  driver.rms_current(600);
+  // driver.rms_current(2000, 0.1);
+  // driver.pwm_autoscale(false);
+  // driver.en_spreadCycle(false);
   driver.microsteps(2);
-  driver.pwm_autoscale(true);
-  driver.en_spreadCycle(true);
   driver.intpol(true);
 
   delay(2000);
 
   if (Serial) Serial.println("Finished Setup");
+
+  readSettings();
+  delay(500);
 }
 
 void loop() {
+  // uint16_t msread=driver.microsteps();
+  // Serial.print(F("Read microsteps via UART to test UART receive : "));    Serial.println(msread); 
+
+  // Serial.println(F("Move steps forward"));
+  // // driver.rms_current(600);
+  // if (direction) {
+  //   digitalWrite(DIR_PIN, HIGH);
+  //   delayMicroseconds(20);
+  // }
+  // int i;
+  // for (i = 0; i < stepLimit; i++) {
+  //   digitalWrite(STEP_PIN, HIGH);
+  //   delayMicroseconds(4);
+  //   digitalWrite(STEP_PIN, LOW);
+  //   delayMicroseconds(nextStepTimeOffset);
+  // }
+  // // stepper.moveToPositionInSteps(6400);
+  // // digitalWrite(DIR_PIN, LOW);
+
+  // if (direction) digitalWrite(DIR_PIN, LOW);
+
+  // Serial.println(F("Wait and switch direction"));
+  // // driver.rms_current(10); 
+  // delay(gapWaitTime);
+
+//  Serial.println(F("Move back to 0 position at 300ma"));
+//  driver.rms_current(300); 
+  // stepper.moveToPositionInSteps(0);
+
+  // for (i = 0; i < 3200; i++) {
+  //   digitalWrite(STEP_PIN, HIGH);
+  //   delayMicroseconds(4);
+  //   digitalWrite(STEP_PIN, LOW);
+  //   delayMicroseconds(600);
+  // }
+  // digitalWrite(DIR_PIN, LOW);
+
+//  Serial.println(F("Automove motor for 2 seconds, then switch direction"));
+  // delay(1000);
+
+  //MOVE MOTOR VIA UART AND CHANGE DIRECTION VIA SOFTWARE, IT RUNS AS LONG AS YOU LET IT... PROBABLY ONLY USEFUL WITH ENCODER. THE VALUE SETS ONLY THE SPEED.
+  // driver.VACTUAL(16000); //SET SPEED OF MOTOR
+  // delay(2000); // MOTOR MOVES 2 SEC THEN STOPS
+  // driver.VACTUAL(0); //STOP MOTOR BY SETTING SPEED TO 0
+  // shaft = !shaft; // REVERSE DIRECTION
+  // driver.shaft(shaft); // SET DIRECTION
   currentStepTime = micros();
 
   if (running) {
@@ -91,9 +149,15 @@ void loop() {
       nextStepTime = currentStepTime + nextStepTimeOffset;
       currentStep++;
       if ((currentStep % stepLimit) == 0) {
-        reverse = !reverse;
-        driver.shaft(reverse);
-        delayMicroseconds(4);
+        direction = !direction;
+        if (Serial) { Serial.print("Bouncing "); Serial.println(direction); }
+        digitalWrite(DIR_PIN, direction ? HIGH : LOW);
+        delayMicroseconds(20);
+
+        if (gapWaitTime > 0) {
+          nextStepTime += gapWaitTime;
+          return;
+        }
       }
 
       digitalWrite(STEP_PIN, HIGH);
@@ -104,8 +168,6 @@ void loop() {
   }
 
   if (nextSerialPollTime <= currentStepTime) {
-    // BLE.poll();
-
     if (Serial && Serial.available()) {
       String command = Serial.readStringUntil('\n');
       handleCommand(command);
@@ -116,118 +178,229 @@ void loop() {
 
 void handleCommand(const String& command) {
   const char chr = command.charAt(0);
-  int microstep = 0;
 
   switch (chr) {
-    case 'X':  // Stopping
-    case 'x':
-      if (Serial) Serial.println("Stopping");
-      running = false;
-      break;
-    case 'O':  // Offset
-    case 'o':
-      nextStepTimeOffset = command.substring(1).toInt();
-      if (Serial) Serial.print("Setting offset to ");
-      if (Serial) Serial.println(nextStepTimeOffset);
-      if (nextStepTimeOffset < 20) nextStepTimeOffset = 20;
-      else if (nextStepTimeOffset > 10000000) nextStepTimeOffset = 10000000;
-      running = true;
-      break;
-    case 'D':  // Switch direction
-    case 'd':
-      reverse = !reverse;
-      if (Serial) Serial.print("Switching direction - ");
-      if (Serial) Serial.println(reverse);
-      driver.shaft(reverse);
-      delayMicroseconds(4);
-      running = true;
-      break;
-    case 'L': // Limit
-    case 'l':
-      stepLimit = command.substring(1).toInt();
-      if (Serial) Serial.print("Setting limit to ");
-      if (Serial) Serial.println(stepLimit);
-      if (stepLimit < 1) stepLimit = 1;
-      else if (stepLimit > 1000000) stepLimit = 1000000;
-      running = true;
-      break;
-    case 'M': // Microstep
-    case 'm':
-      microstep = command.substring(1).toInt();
-      if (Serial) Serial.print("Setting microsteps to ");
-      if (Serial) Serial.println(microstep);
-      if (microstep < 0) microstep = 0;
-      else if (microstep > 256) microstep;
-      driver.microsteps((uint16_t)microstep);
-      break;
-    case 'R':  // Read current settings
-    case 'r':
-      if (!Serial) return;
-      TMC2209_n::IOIN_t i{0}; i.sr = driver.IOIN();
-      TMC2208_n::GCONF_t g{0}; g.sr = driver.GCONF();
-      TMC2208_n::CHOPCONF_t c{0}; c.sr = driver.CHOPCONF();
-      TMC2209_n::COOLCONF_t l{0}; l.sr = driver.COOLCONF();
-      TMC2208_n::PWMCONF_t p{0}; p.sr = driver.PWMCONF();
-      uint16_t rmsCurrent = driver.rms_current();
-
-      Serial.println("Input IO status:");
-      Serial.print("    enn:       "); Serial.println(i.enn);
-      Serial.print("    ms1:       "); Serial.println(i.ms1);
-      Serial.print("    ms2:       "); Serial.println(i.ms2);
-      Serial.print("    diag:      "); Serial.println(i.diag);
-      Serial.print("    pdn_uart:  "); Serial.println(i.pdn_uart);
-      Serial.print("    step:      "); Serial.println(i.step);
-      Serial.print("    spread_en: "); Serial.println(i.spread_en);
-      Serial.print("    dir:       "); Serial.println(i.dir);
-      Serial.print("    version:   "); Serial.println(i.version);
-
-      Serial.println("");
-      Serial.println("General configuration:");
-      Serial.print("    i_scale_analog:   "); Serial.println(g.i_scale_analog);
-      Serial.print("    internal_rsense:  "); Serial.println(g.internal_rsense);
-      Serial.print("    en_spreadcycle:   "); Serial.println(g.en_spreadcycle);
-      Serial.print("    shaft:            "); Serial.println(g.shaft);
-      Serial.print("    index_otpw:       "); Serial.println(g.index_otpw);
-      Serial.print("    index_step:       "); Serial.println(g.index_step);
-      Serial.print("    pdn_disable:      "); Serial.println(g.pdn_disable);
-      Serial.print("    mstep_reg_select: "); Serial.println(g.mstep_reg_select);
-      Serial.print("    multistep_filt:   "); Serial.println(g.multistep_filt);
-
-      Serial.println("");
-      Serial.println("Chopper configuration:");
-      Serial.print("    toff:    "); Serial.println(c.toff);
-      Serial.print("    hstrt:   "); Serial.println(c.hstrt);
-      Serial.print("    hend:    "); Serial.println(c.hend);
-      Serial.print("    tbl:     "); Serial.println(c.tbl);
-      Serial.print("    vsense:  "); Serial.println(c.vsense);
-      Serial.print("    mres:    "); Serial.println(c.mres);
-      Serial.print("    intpol:  "); Serial.println(c.intpol);
-      Serial.print("    dedge:   "); Serial.println(c.dedge);
-      Serial.print("    diss2g:  "); Serial.println(c.diss2g);
-      Serial.print("    diss2vs: "); Serial.println(c.diss2vs);
-
-      Serial.println("");
-      Serial.println("CoolStep configuration:");
-      Serial.print("    semin:  "); Serial.println(l.semin);
-      Serial.print("    seup:   "); Serial.println(l.seup);
-      Serial.print("    semax:  "); Serial.println(l.semax);
-      Serial.print("    sedn:   "); Serial.println(l.sedn);
-      Serial.print("    seimin: "); Serial.println(l.seimin);
-
-      Serial.println("");
-      Serial.println("Pulse-width modulation configuration:");
-      Serial.print("    pwm_ofs:       "); Serial.println(p.pwm_ofs);
-      Serial.print("    pwm_grad:      "); Serial.println(p.pwm_grad);
-      Serial.print("    pwm_freq:      "); Serial.println(p.pwm_freq);
-      Serial.print("    pwm_autoscale: "); Serial.println(p.pwm_autoscale);
-      Serial.print("    pwm_autograd:  "); Serial.println(p.pwm_autograd);
-      Serial.print("    freewheel:     "); Serial.println(p.freewheel);
-      Serial.print("    pwm_reg:       "); Serial.println(p.pwm_reg);
-      Serial.print("    pwm_lim:       "); Serial.println(p.pwm_lim);
-
-      Serial.println("");
-      Serial.print("Requested current: "); Serial.println(rmsCurrent);
-
-      break;
+    case 'X': case 'x':  stop();                    break;
+    case 'O': case 'o':  setStepWaitTime(command);  break;
+    case 'C': case 'c':  setCurrent(command);       break;
+    case 'D': case 'd':  switchDirection();         break;
+    case 'I': case 'i':  switchInterpolation();     break;
+    case 'L': case 'l':  setLimit(command);         break;
+    case 'G': case 'g':  setGapWaitTime(command);   break;
+    case 'M': case 'm':  setMicrostep(command);     break;
+    case 'R': case 'r':  readSettings();            break;
   }
+}
+
+void stop() {
+  if (Serial) Serial.println("Stopping");
+  running = false;
+}
+
+void setStepWaitTime(const String & command) {
+  nextStepTimeOffset = command.substring(1).toInt();
+  if (Serial) Serial.print("Setting offset to ");
+  if (Serial) Serial.println(nextStepTimeOffset);
+  if (nextStepTimeOffset < 20) nextStepTimeOffset = 20;
+  else if (nextStepTimeOffset > 10000000) nextStepTimeOffset = 10000000;
+  running = true;
+}
+
+void setCurrent(const String & command) {
+  int current = command.substring(1).toInt();
+  if (Serial) Serial.print("Setting current to ");
+  if (Serial) Serial.println(current);
+  driver.rms_current(current);
+}
+
+void switchDirection() {
+  direction = !direction;
+  if (Serial) Serial.print("Switching direction - ");
+  if (Serial) Serial.println(direction);
+  digitalWrite(DIR_PIN, direction ? HIGH : LOW);
+  delayMicroseconds(4);
+  running = true;
+}
+
+void switchInterpolation() {
+  bool interpolation = !driver.intpol();
+
+  if (Serial) Serial.print("Switching interpolation - ");
+  if (Serial) Serial.println(interpolation);
+  driver.intpol(interpolation);
+  delayMicroseconds(4);
+}
+
+void setLimit(const String & command) {
+  stepLimit = command.substring(1).toInt();
+  if (Serial) Serial.print("Setting limit to ");
+  if (Serial) Serial.println(stepLimit);
+  if (stepLimit < 1) stepLimit = 1;
+  else if (stepLimit > 1000000) stepLimit = 1000000;
+  running = true;
+}
+
+void setGapWaitTime(const String & command) {
+  gapWaitTime = command.substring(1).toInt();
+  if (Serial) Serial.print("Setting gap wait time to ");
+  if (Serial) Serial.println(gapWaitTime);
+  if (gapWaitTime < 0) gapWaitTime = 0;
+  else if (gapWaitTime > 100000000) gapWaitTime = 100000000;
+  running = true;
+}
+
+void setMicrostep(const String & command) {
+  int microstep = command.substring(1).toInt();
+  if (Serial) Serial.print("Setting microsteps to ");
+  if (Serial) Serial.println(microstep);
+  if (microstep < 0) microstep = 0;
+  else if (microstep > 256) microstep;
+  driver.microsteps((uint16_t)microstep);
+}
+
+void readSettings() {
+  if (!Serial) return;
+
+  uint16_t msread = driver.microsteps();
+  Serial.print("Microsteps read: ");
+  Serial.println(msread);
+
+  uint8_t mres = driver.mres();
+  Serial.print("MRES read: ");
+  Serial.println(mres);
+  // TMC2209_n::IOIN_t i{ 0 };
+  // i.sr = driver.IOIN();
+  // TMC2208_n::GCONF_t g{ 0 };
+  // g.sr = driver.GCONF();
+  // TMC2208_n::CHOPCONF_t c{ 0 };
+  // c.sr = driver.CHOPCONF();
+  // TMC2209_n::COOLCONF_t l{ 0 };
+  // l.sr = driver.COOLCONF();
+  // TMC2208_n::PWMCONF_t p{ 0 };
+  // p.sr = driver.PWMCONF();
+  // uint16_t rmsCurrent = driver.rms_current();
+
+  // displayIoInputs(i);
+  // Serial.println("");
+
+  // displayGeneralConfiguration(g);
+  // Serial.println("");
+
+  // displayChopperConfiguration(c);
+  // Serial.println("");
+
+  // displayCoolStepConfiguration(l);
+  // Serial.println("");
+
+  // displayPulseWidthModulationConfiguration(p);
+  // Serial.println("");
+
+  // displayRequestedCurrent(rmsCurrent);
+}
+
+void displayIoInputs(TMC2209_n::IOIN_t & i) {
+  Serial.println("Input IO status:");
+  Serial.print("    enn:       ");
+  Serial.println(i.enn);
+  Serial.print("    ms1:       ");
+  Serial.println(i.ms1);
+  Serial.print("    ms2:       ");
+  Serial.println(i.ms2);
+  Serial.print("    diag:      ");
+  Serial.println(i.diag);
+  Serial.print("    pdn_uart:  ");
+  Serial.println(i.pdn_uart);
+  Serial.print("    step:      ");
+  Serial.println(i.step);
+  Serial.print("    spread_en: ");
+  Serial.println(i.spread_en);
+  Serial.print("    dir:       ");
+  Serial.println(i.dir);
+  Serial.print("    version:   ");
+  Serial.println(i.version);
+}
+
+void displayGeneralConfiguration(TMC2208_n::GCONF_t & g) {
+  Serial.println("General configuration:");
+  Serial.print("    i_scale_analog:   ");
+  Serial.println(g.i_scale_analog);
+  Serial.print("    internal_rsense:  ");
+  Serial.println(g.internal_rsense);
+  Serial.print("    en_spreadcycle:   ");
+  Serial.println(g.en_spreadcycle);
+  Serial.print("    shaft:            ");
+  Serial.println(g.shaft);
+  Serial.print("    index_otpw:       ");
+  Serial.println(g.index_otpw);
+  Serial.print("    index_step:       ");
+  Serial.println(g.index_step);
+  Serial.print("    pdn_disable:      ");
+  Serial.println(g.pdn_disable);
+  Serial.print("    mstep_reg_select: ");
+  Serial.println(g.mstep_reg_select);
+  Serial.print("    multistep_filt:   ");
+  Serial.println(g.multistep_filt);
+}
+
+void displayChopperConfiguration(TMC2208_n::CHOPCONF_t & c) {
+  Serial.println("Chopper configuration:");
+  Serial.print("    toff:    ");
+  Serial.println(c.toff);
+  Serial.print("    hstrt:   ");
+  Serial.println(c.hstrt);
+  Serial.print("    hend:    ");
+  Serial.println(c.hend);
+  Serial.print("    tbl:     ");
+  Serial.println(c.tbl);
+  Serial.print("    vsense:  ");
+  Serial.println(c.vsense);
+  Serial.print("    mres:    ");
+  Serial.println(c.mres);
+  Serial.print("    intpol:  ");
+  Serial.println(c.intpol);
+  Serial.print("    dedge:   ");
+  Serial.println(c.dedge);
+  Serial.print("    diss2g:  ");
+  Serial.println(c.diss2g);
+  Serial.print("    diss2vs: ");
+  Serial.println(c.diss2vs);
+}
+
+void displayCoolStepConfiguration(TMC2209_n::COOLCONF_t & l) {
+  Serial.println("CoolStep configuration:");
+  Serial.print("    semin:  ");
+  Serial.println(l.semin);
+  Serial.print("    seup:   ");
+  Serial.println(l.seup);
+  Serial.print("    semax:  ");
+  Serial.println(l.semax);
+  Serial.print("    sedn:   ");
+  Serial.println(l.sedn);
+  Serial.print("    seimin: ");
+  Serial.println(l.seimin);
+}
+
+void displayPulseWidthModulationConfiguration(TMC2208_n::PWMCONF_t & p) {
+  Serial.println("Pulse-width modulation configuration:");
+  Serial.print("    pwm_ofs:       ");
+  Serial.println(p.pwm_ofs);
+  Serial.print("    pwm_grad:      ");
+  Serial.println(p.pwm_grad);
+  Serial.print("    pwm_freq:      ");
+  Serial.println(p.pwm_freq);
+  Serial.print("    pwm_autoscale: ");
+  Serial.println(p.pwm_autoscale);
+  Serial.print("    pwm_autograd:  ");
+  Serial.println(p.pwm_autograd);
+  Serial.print("    freewheel:     ");
+  Serial.println(p.freewheel);
+  Serial.print("    pwm_reg:       ");
+  Serial.println(p.pwm_reg);
+  Serial.print("    pwm_lim:       ");
+  Serial.println(p.pwm_lim);
+}
+
+void displayRequestedCurrent(uint16_t rmsCurrent) {
+  Serial.print("Requested current: ");
+  Serial.println(rmsCurrent);
 }
